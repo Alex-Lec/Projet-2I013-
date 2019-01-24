b4d5389ff9e9c616db6115a10fdf6ddb5c77d67f
LECUYER <alexislecuyer14@gmail.com>
2019-01-24 14:49:05 +0100

    premier commit synthese

---
+++ Robot.py
@@ -0,0 +1,22 @@
+class Robot:
+    """
+        Classe Robot :
+        position (x, y)
+        Camera
+        Roue droite, gauche
+        Detecteur
+        Acceleremetre
+    """
+
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+
+    def avancer(self, distance):
+        pass
+
+    def reculer(self, distance):
+        pass
+
+    def tourner(self, angle):
+        pass
