a42e2b02a4c40ad0343ac999439cbf8809503b9e
3681907 <46990568+3681907@users.noreply.github.com>
2019-01-31 09:23:32 +0100

    Update Vecteur.py
    + méthodes add, sub
    redef méthodes produitScalaire, produitVectoriel

--- Vecteur.py
+++ Vecteur.py
@@ -1,10 +1,19 @@
+import numpy as np
+
 class Vecteur():
 	def __init__(self, x, y, z):
-	    self.x = x
-	    self.y = y
-	    self.z = z
-    
-    def produitScalaire(self, v):
-        pass
+		self.x = x
+		self.y = y
+		self.z = z
+	
+	def produitScalaire(self, v):
+        	return np.inner(self, v)
+	
 	def produitVectoriel(self,v):
-		pass
+		return np.cross(self, v)
+	
+	def add(self, v):
+		return Vecteur(self.x + v.x, self.y + v.y, self.z + v.z)
+	
+	def sub(self, v):
+		return Vecteur(self.x - v.x, self.y - v.y, self.z - v.z)
