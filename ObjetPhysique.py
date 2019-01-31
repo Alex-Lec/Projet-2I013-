17ec6c9c0fb27816cd9dbbc415f495e5df37f220
3681907 <46990568+3681907@users.noreply.github.com>
2019-01-31 11:55:20 +0100

    update Terrain et ObjetPhysique
    ajout du systeme de détection de collisions (à tester)

--- ObjetPhysique.py
+++ ObjetPhysique.py
@@ -1,9 +1,10 @@
 class ObjetPhysique:
 
-    def __init__(self, x, y, z, dir):
+    def __init__(self, x, y, z, cote, dir):
         self.x = x
         self.y = y
         self.z = z
+        self.cote = cote
         self.dir = dir
     
     def get_x(self):
@@ -14,6 +15,18 @@ class ObjetPhysique:
     
     def get_z(self):
         return self.z
+    
+    def set_x(self, x):
+        self.x = x
+    
+    def set_y(self, y):
+        self.y = y
+    
+    def set_z(self, z):
+        self.z = z
+    
+    def get_cote(self):
+        return self.cote
     
     def get_dir(self):
         return self.dir
--- Terrain.py
+++ Terrain.py
@@ -1,21 +1,50 @@
 class Terrain():
 
-
-    def __init__(self, x, y):
-        self.x = x;
-        self.y=y;
+    def __init__(self, dimx, dimy):
+        self.dimx = dimx;
+        self.dimy = dimy;
         self.objet = []
 
     def ajouter_robot(self, x,y):
-        pass
+        c = Camera()
+        rg = Roue()
+        rd = Roue()
+        d = Detecteur()
+        a = Accelerometre()
+        dir = Vecteur(0.0, 0.0, 0.0)
+        r = Robot(x, y, dir, c, rd, rg, d, a)
+        self.objet.append(r)
 
-    def ajouter_object(self, x,y):
-        pass
+    def ajouter_objet(self, o):
+        self.objet.append(o)
 
     def afficher_terrain():
         pass
-
-    """
-    def interaction (objet1,objet2):
-        pass
-    """
+    
+    def testCollision(self, i1, i2):
+        o1 = self.objet[i1]
+        o2 = self.objet[i2]
+        ovx = min(o1.get_x() + o1.get_cote(), o2.get_x() + o2.get_cote()) - max(o1.get_x() - o1.get_cote(), o2.get_x() - o2.get_cote()) > 0
+        ovy = min(o1.get_y() + o1.get_cote(), o2.get_y() + o2.get_cote()) - max(o1.get_y() - o1.get_cote(), o2.get_y() - o2.get_cote()) > 0
+        return ovx & ovy
+    
+    def checkCollisions (self):
+        for i in range(len(self.objet)):
+            try:
+                for j in range(len(self.objet)):
+                    if i == j:
+                        continue
+                    if testCollision(i, j):
+                        raise ValueError()
+            except ValueError:
+                o1 = self.objet[i]
+                o2 = self.objet[j]
+                if o1.get_x() + o1.get_cote() < o2.get_x() - o2.get_cote():
+                    o2.set_x(o2.get_x() + (o2.get_x() - o2.get_cote() - o1.get_x() - o1.get_cote()) + 1)
+                else:
+                    o2.set_x(o2.get_x() - (o1.get_x() + o1.get_cote() - o2.get_x() + o2.get_cote()) - 1)
+                if o1.get_y() + o1.get_cote() < o2.get_y() - o2.get_cote():
+                    o2.set_y(o2.get_y() + (o2.get_y() - o2.get_cote() - o1.get_y() - o1.get_cote()) + 1)
+                o1.set_dir(Vecteur(0.0,0.0,0.0).sub(o1.get_dir()))  
+                o2.set_dir(Vecteur(0.0,0.0,0.0).sub(o2.get_dir()))
+            
