5dd870ef3cab592480c6162b56582b04aee836fe
CecileB-G3 <46991677+CecileB-G3@users.noreply.github.com>
2019-01-31 11:47:26 +0100

    Add files via upload

--- Robot.py
+++ Robot.py
@@ -1,6 +1,8 @@
-# -*-coding:utf-8 -*
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
 
-class Robot(ObjetPhysique):
+
+class Robot():#(ObjetPhysique):
     """
     Classe Robot :
     position (x, y);
@@ -11,18 +13,33 @@ class Robot(ObjetPhysique):
     """
 
     def __init__(self, x, y, z, dir, camera, rd, rg, detecteur, accelerometre):
-        ObjetPhysique(x, y, z, dir)
+       # ObjetPhysique(x, y, z, dir)
+        self.x=x
+        self.y=y
+        self.z=z
         self.camera = camera
         self.rd = rd
         self.rg = rg
         self.detecteur = detecteur
         self.accelerometre = accelerometre
+        
+    def printPos(self):
+        print("position : x= ", self.x ," et y = ", self.y)
 
-    def avancer(self, distance):
-        pass
+    def avancer(self, x, y):
+        self.printPos()
+        print("avancer de x= ", x, " et de y = ", y)
+        self.x+=x
+        self.y+=y
+        self.printPos()
+        
 
-    def reculer(self, distance):
-        pass
+    def reculer(self, x, y):
+        self.x=self.x-x
+        self.y=self.y-y
+        
 
     def tourner(self, angle):
         pass
+    
+    
