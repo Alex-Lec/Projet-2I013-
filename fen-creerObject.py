63a0498f37b5152ee3805a2eb8317e6ed8e89333
Lewean <tryalexandre@gmail.com>
2019-01-31 12:18:18 +0100

    Add files via upload

---
+++ fen-creerObject.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+def envoyeObject(fen):
+    fen.quit
+    
+def callback():
+    if askyesno('Titre 1', 'Êtes-vous sûr de vouloir faire ça?'):
+        showwarning('Titre 2', 'Tant pis...')
+    else:
+        showinfo('Titre 3', 'Vous avez peur!')
+        showerror("Titre 4", "Aha")
+
+
+
+
+def creerObject():
+    fen = Tk()
+    fen.title("Ajouter object")
+    fen.geometry("300x500")
+    posx = Entry(fen, x = "x =").pack()
+    posy = Entry(fen, y = "y =").pack()
+    posz = Entry(fen, z = "z =").pack()
+    
+    dimx = Entry(fen, dx = "dim x =").pack()
+    dimy = Entry(fen, dy = "dim y =").pack()
+    dimz = Entry(fen, dz = "dim z =").pack()
+    Button(text='Action', command=callback).pack()
+
+    ok = Button(fen, text = "Ok", command=fen.quit).pack()
+    fen.mainloop()
+    
+    return posx,posy,posz,dimx,dimy,dimz
