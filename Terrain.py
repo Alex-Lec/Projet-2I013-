ac963afac7bf7a49955b996deba2a8c3e72493fe
Lewean <tryalexandre@gmail.com>
2019-01-31 09:51:39 +0100

    Add files via upload

---
+++ Terrain.py
@@ -0,0 +1,21 @@
+class Terrain():
+
+
+    def __init__(self, x, y):
+        self.x = x;
+        self.y=y;
+        self.objet = []
+
+    def ajouter_robot(self, x,y):
+        pass
+
+    def ajouter_object(self, x,y):
+        pass
+
+    def afficher_terrain():
+        pass
+
+    """
+    def interaction (objet1,objet2):
+        pass
+    """
