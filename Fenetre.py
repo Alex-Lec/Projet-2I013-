1a9367de55a287e0dc12661245b3ffc76ae125eb
LECUYER <alexislecuyer14@gmail.com>
2019-02-04 16:54:05 +0100

    fenetre

--- Fenetre.py
+++ Fenetre.py
@@ -33,12 +33,14 @@ class Fenetre():
         menu1.add_command(label = "Quitter", command = self.fenetre.destroy)
         menubar.add_cascade(label = "Fichier", menu = menu1)
         menubar.add_cascade(label = "Options")
+
+        self.arene_canvas = Canvas(self.fenetre, width = 1000, height = 600)
         
         self.fenetre.config(menu = menubar)
 
     def affichage_arene(self):
 
-        self.arene_canvas = Canvas(self.fenetre, width = 1000, height = 600)
+        self.arene_canvas.configure(bg = "white")
 
         for i in self.arene.objet:
             self.arene_canvas.create_rectangle(i.x - i.largeur // 2, i.y - i.longueur // 2, \
@@ -113,19 +115,20 @@ class Fenetre():
     #@staticmethod
     def creerRobot(self):
 
-        def ok_button():
-            self.arene.robot.append(Robot(posx, posy, posz))
+        def ok_button(x, y, z):
+            self.arene.robot.append(Robot(x, y, z))
             fen.destroy()
+            print(x)
 
         fen = Tk()
         fen.title("Ajouter robot")
         fen.resizable(0, 0)
 
         posx = IntVar()
-        posy = IntVar()
+        posy = IntVar()         
         posz = IntVar()
 
-        Label(fen, text = "x =").grid(row = 0, column = 0) #Utilisation d''un tableau pour gérer l'espace
+        Label(fen, text = "x =").grid(row = 0, column = 0)
         Label(fen, text = "y =").grid(row = 0, column = 2)
         Label(fen, text = "z =").grid(row = 0, column = 4)
 
@@ -133,7 +136,7 @@ class Fenetre():
         Entry(fen, textvariable = posy, width = 3).grid(row = 0, column = 3)
         Entry(fen, textvariable = posz, width = 3).grid(row = 0, column = 5)
 
-        ok = Button(fen, text = "Ok", command = lambda : ok_button())
+        ok = Button(fen, text = "Ok", command = lambda : ok_button(posx.get(), posy.get(), posz.get()))
         annuler = Button(fen, text = "Exit", command = fen.destroy)
         
         ok.grid(row = 3, column = 2)
@@ -186,7 +189,7 @@ class Fenetre():
 
 """
 Test rendu graphique arène :
-"""
+
 arene = Terrain()
 arene.objet.append(ObjetPhysique(50, 40, 0, 40, 10))
 arene.objet.append(ObjetPhysique(400, 360, 0, 50, 30))
@@ -194,3 +197,4 @@ arene.objet.append(ObjetPhysique(780, 250, 0, 30, 30))
 arene.robot.append(ObjetPhysique(500, 200, 0, 30, 30))
 fenetre = Fenetre(arene)
 fenetre.affichage_arene()
+"""
