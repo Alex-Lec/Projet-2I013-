8560fa7481d3f029e1fb93105f985f07122b4052
Alexandre <tryalexandregmail.com>
2019-02-07 12:12:38 +0100

    Test branch

--- ObjetPhysique.py
+++ ObjetPhysique.py
@@ -1,36 +1,13 @@
 class ObjetPhysique:
 
-    def __init__(self, x = 0, y = 0, z = 0, longueur = 50, largeur = 100, hauteur = 20):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.largeur = largeur
-        self.longueur = longueur
-        self.hauteur = hauteur
+    def __init__(self, x1,y1,x2,y2,x3,y3,x4,y4):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+        self.x4 = x4
+        self.y4 = y4
+
     
-    def get_x(self):
-        return self.x
-    
-    def get_y(self):
-        return self.y
-    
-    def get_z(self):
-        return self.z
-    
-    def set_x(self, x):
-        self.x = x
-    
-    def set_y(self, y):
-        self.y = y
-    
-    def set_z(self, z):
-        self.z = z
-    
-    def get_dim(self):
-        return (self.longueur, self.largeur, self.hauteur)
-    
-    def get_dir(self):
-        return self.dir
-    
-    def set_dir(self, ndir):
-        self.dir = ndir
