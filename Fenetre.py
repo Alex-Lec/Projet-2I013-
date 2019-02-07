27d346dd0c8dc6cfcf6aef0d383c8477f727bb38
Luc <luc.sterkers@laposte.net>
2019-02-07 11:33:51 +0100

    des robots bougnet

--- Fenetre.py
+++ Fenetre.py
@@ -53,7 +53,7 @@ class Fenetre():
         for i in self.arene.robot: #tous les objets de terrain sont transformés en forme
             rob = self.arene_canvas.create_rectangle(i.x - i.largeur // 2, i.y - i.longueur // 2, \
                 i.x + i.largeur // 2, i.y + i.longueur // 2, fill = "red")
-            self.arene_canvas.create_text(i.x, i.y, text = self.arene.robot.index(i) + 1, fill = "black")
+            self.arene_canvas.create_text(i.x, i.y, text = self.arene.robot.index(i), fill = "black")
             self.listRobots += [rob]
 
     def affichage_arene(self):
@@ -113,9 +113,12 @@ class Fenetre():
     def creerRobot(self):
 
         def ok_button():
-            rob = self.arene.robot.append(Robot(posx.get(), posy.get(), posz.get()))
+            i = Robot(posx.get(), posy.get(), posz.get())
+            self.arene.robot.append(i)
+            
+            rob = self.arene_canvas.create_rectangle(i.x - i.largeur // 2, i.y - i.longueur // 2, \
+                i.x + i.largeur // 2, i.y + i.longueur // 2, fill = "red")
             self.listRobots += [rob]
-            self.affichage_arene()
             fen.destroy()
         
         fen = Toplevel(self.fenetre)
@@ -146,13 +149,9 @@ class Fenetre():
             
             for j in range(pas.get()):
                 self.arene.robot[id_robot.get()].avancer(x.get(), 0)
-                i = self.arene.robot[id_robot.get()]
                 self.arene_canvas.move(self.listRobots[id_robot.get()],x.get(),0)
                 self.arene_canvas.update()
-                time.sleep(.02)
-
-
-             
+                time.sleep(.01)
 
 
         fen = Toplevel(self.fenetre)
