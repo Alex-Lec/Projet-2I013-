7794209ff5636554188963bc63a496d17fa414e8
Luc <luc.sterkers@laposte.net>
2019-03-24 15:43:05 +0100

    Ajout de draw() dans objet physique

--- composant/objetphysique.py
+++ composant/objetphysique.py
@@ -6,7 +6,9 @@ import numpy as np
 from math import radians,sqrt, cos, sin, pi
 class ObjetPhysique:
 
-    def __init__(self, x = 0, y = 0, z = 0, largeur = 50, longueur = 100, hauteur = 20):
+    def __init__(self, x = 0, y = 0, z = 0, \
+                 largeur = 50, longueur = 100, hauteur = 20\
+                 r = 0, g = 50, b = 0):
        
         self.x = x
         self.y = y
@@ -15,7 +17,10 @@ class ObjetPhysique:
         self.largeur = largeur
         self.hauteur = hauteur
         self.v_dir = Vecteur(1., 0., 0.)
-
+        
+        self.r = r
+        self.g = g
+        self.b = b
 
     def get_points(self):
     
@@ -49,3 +54,61 @@ class ObjetPhysique:
             new_points.append([x_new + self.x, y_new + self.y])
         
         return new_points
+        
+        
+    def draw(self):
+        v_y = self.v_dir.y
+        v_x = self.v_dir.x
+        
+        v_xg = -self.v_dir.y
+        v_yg = self.v_dir.x
+        
+        v_xd = self.v_dir.y
+        v_yd = -self.v_dir.x
+        
+        v_xb = -self.v_dir.x
+        v_yb = -self.v_dir.y
+        
+        glBegin(GL_QUADS)
+        glColor3ub(self.r,self.g,self.b)
+        glVertex3f(v_xb*self.longueur/2 + v_xg*self.largeur/2,
+                   v_yb*self.longueur/2 + v_yg*self.largeur/2,
+                   self.z)# point 1
+    
+        glVertex3f(v_xb*self.longueur/2 + v_xg*self.largeur/2,
+                   v_yb*self.longueur/2 + v_yg*self.largeur/2,
+                   self.z + self.hauteur)# point 2
+    
+        glVertex3f(v_x*self.longueur/2 + v_xg*self.largeur/2,
+                   v_y*self.longueur/2 + v_yg*self.largeur/2,
+                   self.z)# point 3
+    
+        glVertex3f(v_x*self.longueur/2 + v_xg*self.largeur/2,
+                   v_y*self.longueur/2 + v_yg*self.largeur/2,
+                   self.z+self.hauteur)# point 4
+                   
+        glVertex3f(v_x*self.longueur/2 + v_xd*self.largeur/2,
+                   v_y*self.longueur/2 + v_yd*self.largeur/2,
+                   self.z)# point 5
+                   
+        glVertex3f(v_x*self.longueur/2 + v_xd*self.largeur/2,
+                   v_y*self.longueur/2 + v_yd*self.largeur/2,
+                   self.z + self.hauteur)# point 6
+                
+        glVertex3f(v_xb*self.longueur/2 + v_xd*self.largeur/2,
+                   v_yb*self.longueur/2 + v_yd*self.largeur/2,
+                   self.z)# point 7
+                   
+        glVertex3f(v_xb*self.longueur/2 + v_xd*self.largeur/2,
+                   v_yb*self.longueur/2 + v_yd*self.largeur/2,
+                   self.z + self.hauteur)# point 8
+                   
+        glEnd()
+                
+                
+                
+                
+                
+                   
+                   
+                   
--- principal/affichage_3D.py
+++ principal/affichage_3D.py
@@ -8,14 +8,16 @@ from pyglet.window import key
 from OpenGL.GLUT import *
 from pyglet.image.codecs.png import PNGImageDecoder
 
-class Window(pyglet.window.Window):
+class Affichage_3D(pyglet.window.Window):
     xRotation = yRotation = 0
     increment = 5
     toDraw = []
 
     def __init__(self, width, height, title='Test'):
-        super(Window, self).__init__(width, height, title)
+        super(Affichage_3D, self).__init__(width, height, title)
+        self.zoom = 20
         self.setup()
+
 
     def setup(self):
         # One-time GL setup
@@ -23,13 +25,13 @@ class Window(pyglet.window.Window):
         glColor3f(1, 0, 0)
         glEnable(GL_DEPTH_TEST)
         # using Projection mode
-        glViewport(0, 0, super(Window, self).width*2, \
-        super(Window, self).height*2) # taille de la scene
+        glViewport(0, 0, super(Affichage_3D, self).width*2, \
+        super(Affichage_3D, self).height*2) # taille de la scene
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         # perspective
-        aspectRatio = super(Window, self).width / \
-        super(Window, self).height
+        aspectRatio = super(Affichage_3D, self).width / \
+                      super(Affichage_3D, self).height
         gluPerspective(35*self.zoom, aspectRatio, 1, 1000)
         #
         glMatrixMode(GL_MODELVIEW)
@@ -44,19 +46,19 @@ class Window(pyglet.window.Window):
         glEnable(GL_LIGHT1)
         # Define a simple function to create ctypes arrays of floats:
 
-    def vec(*args):
-        return (GLfloat * len(args))(*args)
+        def vec(*args):
+            return (GLfloat * len(args))(*args)
 
-    glLightfv(GL_LIGHT0, GL_POSITION, vec(.5, .5, 1, 0))
-    glLightfv(GL_LIGHT0, GL_SPECULAR, vec(.5, .5, 1, 1))
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, vec(1, 1, 1, 1))
-    glLightfv(GL_LIGHT1, GL_POSITION, vec(1, 0, .5, 0))
-    glLightfv(GL_LIGHT1, GL_DIFFUSE, vec(.5, .5, .5, 1))
-    glLightfv(GL_LIGHT1, GL_SPECULAR, vec(1, 1, 1, 1))
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, \
-    vec(0.5, 0.5, 0.5, 1))
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, vec(1, 1, 1, 1))
-    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50)
+        glLightfv(GL_LIGHT0, GL_POSITION, vec(.5, .5, 1, 0))
+        glLightfv(GL_LIGHT0, GL_SPECULAR, vec(.5, .5, 1, 1))
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, vec(1, 1, 1, 1))
+        glLightfv(GL_LIGHT1, GL_POSITION, vec(1, 0, .5, 0))
+        glLightfv(GL_LIGHT1, GL_DIFFUSE, vec(.5, .5, .5, 1))
+        glLightfv(GL_LIGHT1, GL_SPECULAR, vec(1, 1, 1, 1))
+        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, \
+        vec(0.5, 0.5, 0.5, 1))
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, vec(1, 1, 1, 1))
+        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50)
 
     def on_draw(self):
         # Clear the current GL Window
