7200f8ed06439c90fed880b0d931d48ff4e2f23b
Luc <luc.sterkers@laposte.net>
2019-03-28 11:45:49 +0100

    modif

--- strategie/stratAvance.py
+++ strategie/stratAvance.py
@@ -6,14 +6,22 @@ class StratAvance:
         self.vit = vitesse
     
     def start(self):
-        self.robot.set_motor_dps(1 , self.vit)
+        self.robot.set_motor_dps(1, self.vit)
         self.robot.set_motor_dps(2, self.vit)
         self.robot.offset_motor_encoder(1, self.robot.get_motor_position()[0])
         self.robot.offset_motor_encoder(2, self.robot.get_motor_position()[1])
         #self.robot.last_up = time.time()
     
     def step(self):
-        pass
+        pos1 =selg.robot.get_motor_position()[0] 
+        pos2 = selg.robot.get_motor_position()[1]
+        if (pos1 > pos2):
+            print("declage gauche")
+            #self.robot.set_motor_dps(1, self.vit + pos1-pos2)
+        if (pos1 < pos2):
+            print("declage droit")
+            #self.robot.set_motor_dps(1, self.vit + pos1-pos2)
+            
     
     def stop(self):
         if((self.robot.get_motor_position()[0]*self.robot.WHEEL_CIRCUMFERENCE)
