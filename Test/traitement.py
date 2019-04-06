a6d215d9fdc853dab3d8d293f23e260da6312473
Alexandre <tryalexandregmail.com>
2019-04-06 18:01:16 +0200

    Traitement image

--- Test/traitement.py
+++ Test/traitement.py
@@ -2,13 +2,13 @@ from PIL import Image
 import random
 import numpy as np
 
-filtre1 = [[0][1][0]
+"""filtre1 = [[0][1][0]
            [1][1][1]
            [0][1][0]]
 
 filtre2 = [[1][1][1]
            [1][1][1]
-           [1][1][1]]
+           [1][1][1]]"""
 
 def tret_rec(img,M,x,y):
     cnt = 0
@@ -29,21 +29,21 @@ def tret_rec(img,M,x,y):
         if (M[x+1][y] == 0 and r >= 250 and g<=0 and b<=0):
             M[x+1][y] = 1
             cnt = cnt + 1 + tret_rec(img,M,x+1,y)
-  
+
     if (y+1<img.size[1]):
         (r,g,b) = img.getpixel((x,y+1))
         if (M[x][y+1] == 0 and r >= 250 and g<=0 and b<=0):
             M[x][y+1] = 1
             cnt = cnt + 1 + tret_rec(img,M,x,y+1)
-    
+
     return cnt
 
 
 def traitement(img):
     (dimx, dimy) = img.size
     max = (-1,-1,-1)
-    
-    for i in range(dimx):   
+
+    for i in range(dimx):
         for j in range(dimy):
             (r,g,b) = img.getpixel((i,j))
             if (r >= 250 and g<=0 and b<=0):
@@ -52,7 +52,6 @@ def traitement(img):
                 tmp = tret_rec(img,M,i,j)
                 if (tmp > max[0]):
                     max = (tmp, i, j)
-                    
-                    
-    return max
 
+
+    return max
