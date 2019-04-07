8a710291c4c1b41efdb0bf7e3784dbb2e09c970a
Luc <luc.sterkers@laposte.net>
2019-04-07 10:15:55 +0200

    traitement

--- Test/Test_image.py
+++ Test/Test_image.py
@@ -8,16 +8,17 @@ if __name__ == '__main__':
     img = Image.new("RGB", (d, d), "white")
     for i in range(d):
         for j in range(d):
-            img.putpixel((i,j), (0,30,50)) 
+            img.putpixel((i,j), (0,0,0)) 
+    
     
     for i in range(15000):
         img.putpixel((random.randint(0,d-1),random.randint(0,d-1)), \
-        (random.randint(0,255), random.randint(0,255), random.randint(0,255)))
-    
-    for i in range(50):
-        for j in range(50):
-            img.putpixel((i+50,j+50),(255,0,0))
-   
+        (random.randint(0,255), random.randint(0,20), random.randint(0,20)))
+    """
+    for i in range(120):
+        for j in range(100):
+            img.putpixel((i+300,j+200),(255,0,0))
+    """
           
     img.save("Texture2.jpeg", "jpeg")
     print(img.getpixel((10,10)))
