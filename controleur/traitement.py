f3354d4b72d218cef5c63751fd0e6eb344a1a6f3
Alexandre <tryalexandregmail.com>
2019-04-18 10:57:08 +0200

    Traitement centre

--- controleur/traitement.py
+++ controleur/traitement.py
@@ -1,106 +1,85 @@
-from PIL import Image
-import random
+import cv2
 import numpy as np
+import time
+
+img = cv2.imread('screen_8.jpeg')
+(dimy, dimx,unused) = img.shape
 
 
-def tret3D(img,a,b):
-    L = [(a,b)]
-    M = np.zeros((img.size[0],img.size[1]),int)
-    M[a][b] = 1
-    
-    for (x,y) in L:
-        if (x-1>0):
-            (r,g,b) = img.getpixel((x-1,y))
-            if (M[x-1][y] == 0):
-                if (r >= 200 and g<=0 and b<=0):
-                    L += [(x-1,y)]
-                    M[x-1][y] = 1
-                else : break
-        else : break
-                
-        if (y-1>0):
-            (r,g,b) = img.getpixel((x,y-1))
-            if (M[x][y-1] == 0):
-                if (r >= 200 and g<=0 and b<=0):
-                    L += [(x,y-1)]
-                    M[x][y-1] = 1
-                else : break
-        else : break
-        
-        if (x+1<img.size[0]):
-            (r,g,b) = img.getpixel((x+1,y))
-            if (M[x+1][y] == 0):
-                if (r >= 200 and g<=0 and b<=0):
-                    L += [(x+1,y)]
-                    M[x+1][y] = 1
-                else : break
-        else : break
-        
-        
-        if (y+1<img.size[1]):
-            (r,g,b) = img.getpixel((x,y+1))
-            if (M[x][y+1] == 0):
-                if (r >= 200 and g<=0 and b<=0):
-                    L += [(x,y+1)]
-                    M[x][y+1] = 1
-                else : break
-        else : break  
-    
-    p = np.poly1d([2,2,1-len(L)])
-    return np.floor(p.roots[1])
 
-def traitement_Champi(img):
-    (dimx, dimy) = img.size
-    max = (-1,-1,-1)
-    
-    t = time.time()
-    for i in range(dimx):   
+def trait_red(img,x,y):
+    cpt = 0
+    if (x+2<dimx):
+        (b,g,r) = img[y,x+2]
+    while (x+2<dimx  and (r >= 150 and g<=100 and b<=100) ):
+        (b,g,r) = img[y,x+2]
+        x+=2
+        cpt+=1
+    return cpt
+
+def trait_green(img,x,y):
+    cpt = 0
+    if (x+2<dimx):
+        (b,g,r) = img[y,x+2]
+    while (x+2<dimx  and (g>=110 and g > r +40 and g > b +40) ):
+        (b,g,r) = img[y,x+2]
+        x+=2
+        cpt+=2
+    return cpt
+
+"""def traitement_red(img):
+    max_r=(-1,-1)
+    for i in range(dimx):
         for j in range(dimy):
-        
-            (r,g,b) = img.getpixel((i,j))
-            if (r >= 230 and g<=0 and b<=0):
-                tmp = tret3D(img,i,j)
-                if (tmp > max[0]):
-                    max = (tmp, i, j)
-    
-    print(time.time() - t)
-                    
-    return max
+            (b,g,r) = img[j,i]
+            if (r >= 150 and g<=100 and b<=100):
+                tmp_r = (trait_red(img,i,j),i)
+                i+= tmp_r[0]
+                if (tmp_r[0] > max_r[0]):
+                    max_r = tmp_r
+    return max_r[1]+max_r[0]//2
 
-def traitement_Moy(img):
-    (dimx, dimy) = img.size
-    res = 0
-    open = False
-    cnt = 0
-    deb = 0
-    nbp = 0
-    #t = time.time()
-    for i in range(dimy):   
-        for j in range(dimx):
-            (r,g,b) = img.getpixel((j,i))
-            
-            if (r >= 230 and g<20 and b<20):
-                if not open:
-                    open = True
-                    cnt = 1
-                    deb = j  
-                else:
-                    cnt += 1
-            elif open :
-                res /= cnt
-                res += (j+deb)/2
-                open = False
-                nbp += cnt
-    
-    #print (time.time() - t)      
-    return (res,nbp)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+def traitement_green(img):
+    max =(-1,-1)
+    for i in range(0,dimx,2):
+        for j in range(0,dimy,2):
+            (b,g,r) = img[j,i]
+            if (g>=110 and g > r +40 and g > b +40):
+                tmp = (trait_green(img,i,j),i)
+                i+= tmp[0]
+                if (tmp[0] > max[0]):
+                    max = tmp
+    return max[1]+max[0]//2"""
+
+
+
+def traitement(img):
+    max_r=(-1,-1)
+    max_g=(-1,-1)
+
+    for i in range(0,dimx,2):
+        for j in range(0,dimy,2):
+            (b,g,r) = img[j,i]
+            if (r >= 150 and g<=100 and b<=100):
+                tmp_r = (trait_red(img,i,j),i)
+                i += tmp_r[0]
+                if (tmp_r[0] > max_r[0]):
+                    max_r = tmp_r
+            if (g>=110 and g > r +40 and g > b +40):
+                tmp_g = (trait_green(img,i,j),i)
+                i+= tmp_g [0]
+                if (tmp_g[0] > max_g[0]):
+                    max_g = tmp_g
+
+    return ((max_r[1]+max_r[0]//2) +(max_g[1]+max_g[0]//2)) //2
+
+def ligne(img,x):
+    for i in range (3):
+        for j in range (dimy):
+            img[j,x-1+i] =[0,0,0]
+
+
+ligne(img,traitement(img))
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
